a6fdda9848929b03fc407d085234185a09a0b9cd
Денис Петровский <petrovsky.dv@gmail.com>
2021-03-14 17:15:36 +0300

    удалил лишние комментарии, оптимизировал импорты

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,9 +1,9 @@
-from datacenter.models import Visit
 from django.shortcuts import render
+
+from datacenter.models import Visit
 
 
 def storage_information_view(request):
-    # Программируем здесь
     non_closed_visits = []
     visits = Visit.objects.filter(leaved_at__isnull=True)
     for visit in visits:
@@ -16,6 +16,6 @@ def storage_information_view(request):
             }
         )
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
